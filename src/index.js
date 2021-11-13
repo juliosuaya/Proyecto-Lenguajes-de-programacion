b9e20414c6c0ef72d4a537a3eed48e76447daac1
juliosuaya <jcsl19@hotmail.com>
2021-11-13 18:36:27 -0300

    Biconditional implementation. Code improvements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-const {Prop} = require("./Prop");
-const { Conjunction, Operator, Variable } = require("./operators");
+const { Prop, Conjunction, Variable, Biconditional, Conditional, Disjunction } = require("./props");
 
 const props = new Prop();
 const and = new Conjunction(new Variable("p"), new Variable("q"));
-const value = { p: true, q: false }
-const res = props.evalProp(and, value);
-console.log(res);
+const biconditional = new Biconditional(and, new Variable("q"));
+const conditional = new Conditional(biconditional, and);
+const disjunction = new Disjunction(conditional, biconditional);
+const value = { p: true, q: false };
+console.log(props.evalProp(and, value));
+console.log(props.evalProp(biconditional, value));
+console.log(props.evalProp(conditional, value));
+console.log(props.evalProp(disjunction, value));
